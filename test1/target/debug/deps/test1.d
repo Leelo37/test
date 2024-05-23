b7095e62e1312c177f1984ecc551681e4acb627d
Leelo37 <141325090+Leelo37@users.noreply.github.com>
2024-05-24 01:19:20 +0200

    Add files via upload

---
+++ test1/target/debug/deps/test1.d
@@ -0,0 +1,5 @@
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\deps\test1.exe: src\main.rs
+
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\deps\test1.d: src\main.rs
+
+src\main.rs:
---
+++ test1/target/debug/test1.d
@@ -0,0 +1,1 @@
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\test1.exe: C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\src\main.rs
