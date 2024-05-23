4962625eadbc330d5fa1a83f9df289a7a9dd572a
Leelo37 <lev.bergoc@gmail.com>
2024-05-24 01:22:39 +0200

    cargo namesto world

--- test1/target/debug/deps/test1-073aace85125ed4e.d
+++ test1/target/debug/deps/test1-073aace85125ed4e.d
@@ -1,5 +1,5 @@
-C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\deps\libtest1-073aace85125ed4e.rmeta: src\main.rs
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test\test1\target\debug\deps\libtest1-073aace85125ed4e.rmeta: src\main.rs
 
-C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\deps\test1-073aace85125ed4e.d: src\main.rs
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test\test1\target\debug\deps\test1-073aace85125ed4e.d: src\main.rs
 
 src\main.rs:
--- test1/target/debug/deps/test1-e483d63a38daa952.d
+++ test1/target/debug/deps/test1-e483d63a38daa952.d
@@ -1,5 +1,5 @@
-C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\deps\libtest1-e483d63a38daa952.rmeta: src\main.rs
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test\test1\target\debug\deps\libtest1-e483d63a38daa952.rmeta: src\main.rs
 
-C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test1\target\debug\deps\test1-e483d63a38daa952.d: src\main.rs
+C:\Users\levbe\Documents\Lev\Fakulteta\programiranje2\test\test1\target\debug\deps\test1-e483d63a38daa952.d: src\main.rs
 
 src\main.rs:
